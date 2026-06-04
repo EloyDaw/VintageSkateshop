e0fe31a5af90e9165529164390f206e2aca114b7
Eloy <eloyperezinglada@gmail.com>
2026-06-04 23:00:40 +0200

    Fix Dockerfile y start.sh

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+set -e
+
+php artisan key:generate --force --no-interaction || true
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
 php artisan migrate --force
+
 /start.sh
